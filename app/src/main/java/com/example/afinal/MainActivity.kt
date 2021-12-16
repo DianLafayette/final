c7e6888b1ca5186bd5ab487a3509f2eba9556b5c
Notnik_kg <ulanovadanaya@gmail.com>
2021-12-16 14:48:16 +0600

    есть небольшая MotionLayout анимация на отдельном экране и переход на второй экран через сердечко

--- app/src/main/java/com/example/afinal/MainActivity.kt
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.afinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.afinal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.heart.setOnClickListener {
+            startActivity(Intent(this,AnimationActivity::class.java))}}}
